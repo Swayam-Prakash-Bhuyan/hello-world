fa541ee94f1efbc914d43305e6df67380bb60cfe
Swayam Prakash Bhuyan <swayamprakashbhuyan@gmail.com>
2020-08-28 16:47:03 +0530

    added a callback func in app.listen

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/hello',(req,res) => {
-    res.send('hello from index.js')
+app.get('/',(req,res) => {
+    res.send('hello from server')
 })
 
-app.listen(3000);
+app.listen(3000,(req,res) => {
+    console.log("Server is running on port: 3000")
+});
